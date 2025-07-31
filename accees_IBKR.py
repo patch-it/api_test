e67d7bdfabb575f54de5cec0487e5fca46fd2be4
patch-it <christoffer159@gmail.com>
2025-07-31 15:50:04 +0200

    final ruff of the day

--- accees_IBKR.py
+++ accees_IBKR.py
@@ -29,9 +29,7 @@ session.verify = False
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 session = requests.Session()
 session.verify = False
-session.cookies.set(
-    "JSESSIONID", "your_cookie_here"
-)
+session.cookies.set("JSESSIONID", "your_cookie_here")
 
 # TODO could move all urls to a config file
 
@@ -139,7 +137,6 @@ def check_market(tickers: list):
             continue
 
         data = loop_market_data(conid, keys_to_check)
-
 
         # TODO expand data points and check for data before calling get
         if isinstance(data, list):
@@ -249,7 +246,7 @@ def get_account_balances(currency: Literal["EUR", "USD"]):
 
     if response.status_code == 200:
         data = response.json()
-        #print(f"Money to spend: {currency} - ", data[currency]["cashbalance"])
+        # print(f"Money to spend: {currency} - ", data[currency]["cashbalance"])
         return data[currency]["cashbalance"]
     else:
         print(f"Failed to get account summary: {response.status_code} {response.text}")
@@ -295,14 +292,15 @@ def currency_exchange(from_currency: str, to_currency: str, amount: float = 0.0)
         print(f"Failed to convert currency: {response.status_code} - {response.text}")
         return False
 
+
 def loop_market_data(conid: str, keys_to_check: list):
-    '''Loops through market data requests for a given contract ID (conid) until the required keys are found or maximum attempts are reached.
+    """Loops through market data requests for a given contract ID (conid) until the required keys are found or maximum attempts are reached.
     Args:
         conid (str): The contract ID of the stock.
         keys_to_check (list): List of keys to check in the market data.
     Returns:
         list: A list of dictionaries containing market data if the required keys are found, otherwise returns the first dict in data.
-      '''  
+    """
     for attempt in range(10):
         data = get_market_data(conid)
 
@@ -310,14 +308,16 @@ def loop_market_data(conid: str, keys_to_check: list):
             return data[0]
 
         if not isinstance(data, list) or not all(isinstance(d, dict) for d in data):
-            print(f"Unexpected data format expected a list of dictionaries but got {type(data)}")
+            print(
+                f"Unexpected data format expected a list of dictionaries but got {type(data)}"
+            )
             return data[0]
 
         if any(all(key in d for key in keys_to_check) for d in data):
             return data
 
         # Check if any dict contains both '31' and '70'
-        found = any('31' in d and '70' in d for d in data)
+        found = any("31" in d and "70" in d for d in data)
         if found:
             return data
 
@@ -330,29 +330,29 @@ if __name__ == "__main__":
     try:
         check_session()
     except ConnectionError as e:
-        print(f"Connection to IBKR Client Portal failed: {e}\nAttempting to start IB Gateway...")
+        print(
+            f"Connection to IBKR Client Portal failed: {e}\nAttempting to start IB Gateway..."
+        )
 
         try:
             # Set execute permissions for the script
-            st = os.stat('./start_gateway_and_open.sh')
-            os.chmod('./start_gateway_and_open.sh', st.st_mode | stat.S_IEXEC)
+            st = os.stat("./start_gateway_and_open.sh")
+            os.chmod("./start_gateway_and_open.sh", st.st_mode | stat.S_IEXEC)
 
             result = subprocess.run(
                 ["./start_gateway_and_open.sh"],
                 capture_output=True,
                 text=True,
-                check=True
-        )
+                check=True,
+            )
             print(result.stdout)
             if result.stderr:
                 print(f"Error: {result.stderr}")
-        except (PermissionError) as e:
-            print(f"Permission error: {e}\nMake sure the script has execute permissions. " \
-            "\nSetting execute permissions for start_gateway_and_open.sh...")
-
-            
-            
-
+        except PermissionError as e:
+            print(
+                f"Permission error: {e}\nMake sure the script has execute permissions. "
+                "\nSetting execute permissions for start_gateway_and_open.sh..."
+            )
 
     # Check status for account
     print("Auth status:", get_response("/api/iserver/auth/status").json())
